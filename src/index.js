6236ef359ca4b1f5db37382b39d38ca28e9002d5
Ilyes1 <ihannouch7@gmail.com>
2021-08-08 22:31:55 +0100

    my changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const serverless = require('serverless-http')
+const request = require('request')
 
 const app = express()
 const router = express.Router()
 
+// app.use(express.static('public'))
+
 router.get('/', (req, res) => {
-    res.send('Hello from netlify')
+    res.send('index no problem')
+})
+
+router.get('/weather', (req, res) => {
+    request('http://api.openweathermap.org/data/2.5/weather?q=casablanca&appid=923401310fb8f2a8c1dcb3796e756328', (err, weather) => {
+        res.send(weather)
+    })
 })
 
 app.use('/.netlify/functions/index', router)
