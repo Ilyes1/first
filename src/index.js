0eeb686d2beba1d55cfdc5c65ac0f5a73cd51b88
Ilyes1 <ihannouch7@gmail.com>
2021-08-08 23:17:01 +0100

    my changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const request = require('request')
+const bodyParser = require('body-parser')
 
 const app = express()
 const router = express.Router()
 
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 router.get('/', (req, res) => {
     res.send('index no problem')
 })
 
-router.get('/weather', (req, res) => {
-    request('http://api.openweathermap.org/data/2.5/weather?q=casablanca&appid=923401310fb8f2a8c1dcb3796e756328', (err, weather) => {
+router.post('/weather', (req, res) => {
+    const city = req.body.city
+    request(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=923401310fb8f2a8c1dcb3796e756328`, (err, weather) => {
         res.send(weather)
     })
 })
