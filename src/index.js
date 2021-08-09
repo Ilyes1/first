6cf40637dd4d04346862e57b39d79bbb97e3711b
Ilyes1 <ihannouch7@gmail.com>
2021-08-09 09:46:08 +0100

    my changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const serverless = require('serverless-http')
 const request = require('request')
 const bodyParser = require('body-parser')
-const multer = require('multer')
-const path = require('path')
 
 const app = express()
 const router = express.Router()
@@ -14,20 +12,10 @@ router.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
-    }
+router.get('/:city', (req, res) => {
+    res.send(req.params.city)
 })
 
-const upload = multer({ storage: storage })
-
-router.post('/upload', upload.single('image'), (req, res) => {
-    res.send('File Uploaded.' + req.file.filename)
-})
 
 app.use('/.netlify/functions/index', router)
 
