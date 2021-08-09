af410a9c47c0ebd68ad8a5a3736b7dd6d145849d
Ilyes1 <ihannouch7@gmail.com>
2021-08-09 07:42:43 +0100

    my changes

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../uploads')
+        cb(null, 'uploads')
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname)
